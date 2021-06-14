559a23e221dc763b59d1cbb36a9ea4789c62c3e5
HOOROOBABY <amy200006250625@gmail.com>
2021-06-14 19:36:43 +0800

    0614 dimdian

--- src/app/dimdian/dimdian.page.ts
+++ src/app/dimdian/dimdian.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-dimdian',
@@ -10,6 +11,7 @@ export class DimdianPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
   openIMG(){
@@ -24,6 +26,21 @@ export class DimdianPage implements OnInit {
       document.getElementById('oImg2').style.display = "block";
     else if(document.getElementById('oImg2').style.display = "block")
       document.getElementById('oImg2').style.display = "none";
+  }
+  // 記住地點：將地址回傳至local storage
+  label(){
+    var address = $('#address').text()
+    console.log(address)
+    for (var i = 0, len = localStorage.length; i < len; i++) {
+      if (localStorage.getItem('test'+i) == address) {
+        alert('此地點已存在。');
+        break;
+      } 
+      else {
+        alert('已記住地點！');
+        localStorage.setItem(('test'+(localStorage.length-1)), address);
+      }
+    }   
   }
 
 }
